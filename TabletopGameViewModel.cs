ca0d4f22950a81fc677b931da15e124589ef80b3
i.gritsienko <talen1298@mail.ru>
2022-07-07 11:11:57 +0700

    Добавьте файлы проекта.

---
+++ TabletopGameViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabletopStore
+{
+    public class TabletopViewModel : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private readonly TabletopGame _tabletopGame;
+
+        public TabletopViewModel(TabletopGame tabletopGame)
+        {
+            _tabletopGame = tabletopGame;
+        }
+
+        public string this[string columnName]
+        { 
+            get
+            {
+                string error = String.Empty;
+                switch (columnName)
+                {
+                    case nameof(Price):
+                        if (_tabletopGame.Price < 0)
+                        {
+                            error = "Отрицательная цена";
+                        }
+                        break;
+                }
+                return error;
+            }
+        }
+       
+        public string Name
+        {
+            get { return _tabletopGame.Name; }
+            set
+            {
+                _tabletopGame.Name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string Brand
+        {
+            get { return _tabletopGame.Brand; }
+            set
+            {
+                _tabletopGame.Brand = value;
+                OnPropertyChanged(nameof(Brand));
+            }
+        }
+
+        public int Price
+        {
+            get { return _tabletopGame.Price; }
+            set
+            {
+                _tabletopGame.Price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+
+        public string ImagePath
+        {
+            get { return _tabletopGame.ImagePath; }
+            set
+            {
+                _tabletopGame.ImagePath = value;
+                OnPropertyChanged(nameof(ImagePath));
+            }
+        }
+
+        public string Description
+        { 
+            get { return _tabletopGame.Description;} 
+            set
+            {
+                _tabletopGame.Description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+        public string Error => throw new NotImplementedException();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            //if (PropertyChanged != null)
+            //    PropertyChanged(this, new PropertyChangedEventArgs(prop));
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabletopStore
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        private TabletopViewModel selectedGame;
+
+        public ObservableCollection<TabletopViewModel> Tabletops { get; }
+        public ObservableCollection<TabletopViewModel> futureTabletops { get; }
+
+        public TabletopViewModel SelectedGame
+        {
+            get { return selectedGame; }
+            set
+            {
+                selectedGame = value;
+                OnPropertyChanged(nameof(SelectedGame));
+            }
+        }
+
+        public ViewModel()
+        {
+            Tabletops = new ObservableCollection<TabletopViewModel>
+            {
+                new TabletopViewModel(new TabletopGame { Name = "Tyrants of the Underdark", Brand = "Wizards of the Coast",
+                    Price = 3000, Description = string.Empty, ImagePath = "/Images/Tyrant.jpg" }),
+                new TabletopViewModel(new TabletopGame { Name = "Dungeon Mayhem", Brand = "Dungeons & Dragons",
+                    Price = 2500, Description = string.Empty, ImagePath = "/Images/DungeonMayhem.jpg" }),
+                new TabletopViewModel(new TabletopGame { Name = "Tomb of Annihilation", Brand = "WizKids",
+                    Price = 4200, Description = string.Empty, ImagePath = "/Images/TombOfAnnihilation.jpg" }),
+            };
+
+            futureTabletops = new ObservableCollection<TabletopViewModel>
+            {
+                new TabletopViewModel(new TabletopGame { Name = "Avalon Hill", Brand = "Avalon Hill",
+                    Price = 0, Description = "Never Split the Party…Unless Someone is the Traitor!", ImagePath = "/Images/AvalonHill.jpg" }),
+                new TabletopViewModel(new TabletopGame { Name = "Dragonfire", Brand = "Bushiroad",
+                    Price = 0, Description = "Dragon fire is a 3 to 6 player cooperative deck builder game set on this planet’s greatest roleplaying game setting, Dungeons Dragons",
+                    ImagePath = "/Images/Dragonfire.jpg" }),
+            };
+        }
+
+        // команда добавления нового объекта
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??= new RelayCommand(obj =>
+                  {
+                      TabletopViewModel tabletopGame = new TabletopViewModel(new TabletopGame());
+                      Tabletops.Insert(0, tabletopGame);
+                      SelectedGame = tabletopGame;
+                  });
+            }
+        }
+
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??= new RelayCommand(obj =>
+                    {
+                        if (obj is TabletopViewModel tabletopGame)
+                        {
+                            Tabletops.Remove(tabletopGame);
+                        }
+                    },
+                    (obj) => Tabletops.Count > 0);
+            }
+        }
+
+        private RelayCommand doubleCommand;
+        public RelayCommand DoubleCommand
+        {
+            get
+            {
+                return doubleCommand ??= new RelayCommand(obj =>
+                {
+                    if (obj is TabletopViewModel tabletopGame)
+                    {
+                        TabletopViewModel phoneCopy = new TabletopViewModel(new TabletopGame
+                        {
+                            //Company = tabletopGame.Company,
+                            //Price = tabletopGame.Price,
+                            //Title = tabletopGame.Title
+                        });
+
+                        Tabletops.Insert(0, phoneCopy);
+                    }
+                });
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
